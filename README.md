dd50153100ad94a1fdf71bf78e2ced2239e63227
Eric Peterson <eric@elpete.com>
2017-11-10 08:14:02 -0700

    Change to ColdBox Elixir

--- README.md
+++ README.md
@@ -1,67 +1,72 @@
-# laravel-elixir-postcss
-[![Build Status](https://travis-ci.org/tureki/laravel-elixir-postcss.svg?branch=master)](https://travis-ci.org/tureki/laravel-elixir-postcss)
-[![npm Version](https://img.shields.io/npm/v/laravel-elixir-postcss.svg)](https://www.npmjs.com/package/laravel-elixir-postcss)
-[![npm License](https://img.shields.io/npm/l/laravel-elixir-postcss.svg)](https://www.npmjs.com/package/laravel-elixir-postcss)
+# coldbox-elixir-postcss
+[![npm Version](https://img.shields.io/npm/v/coldbox-elixir-postcss.svg)](https://www.npmjs.com/package/coldbox-elixir-postcss)
+[![npm License](https://img.shields.io/npm/l/coldbox-elixir-postcss.svg)](https://www.npmjs.com/package/coldbox-elixir-postcss)
 
-This Laravel Elixir extension allows you easy to compile PostCSS.
-
-If this package helpful and save your time. Do not forget star it :)
+This ColdBox Elixir extension allows you easy to compile PostCSS.
 
 
 ## Installation
 
 ```bash
-$ npm install laravel-elixir-postcss --save-dev
+$ npm install coldbox-elixir-postcss --save-dev
 ```
 
 
 ## Usage
 
-```javascript
-elixir(function(mix) {
-  //app.css, *.css, **/*.css
-   elixir.postcss('app.css');
-});
+Require this extension at the top of your `gulpfile.js`:
+
+```js
+require( "coldbox-elixir-postcss" );
+```
+
+Then you have access to the `postcss` function inside ColdBox Elixir:
+
+```js
+elixir( function( mix ) {
+    // app.css, *.css, **/*.css
+    mix.postcss( "app.css" );
+} );
 ```
 
 #### Using PostCSS Plugins
 
-```javascript
+```js
 elixir(function(mix) {
-  mix.postcss('app.css', {
-    plugins:[
-      require('postcss-nested')
-    ]
-  });
-});
+    mix.postcss( "app.css", {
+        plugins: [
+            require( "postcss-nested" )
+        ]
+    } );
+} );
 ```
 
 #### Using Other Parser
 
 You can set the [options](https://github.com/postcss/postcss#options) using other parsers like `scss`, `sugarss` etc.
 
-```javascript
-elixir(function(mix) {
-  mix.postcss('app.css', {
-    options: {
-      parser: require('postcss-scss')
-    },
-  });
-});
+```js
+elixir( function( mix ) {
+    mix.postcss( "app.css", {
+        options: {
+            parser: require( "postcss-scss" )
+        }
+    } );
+} );
 ```
 
 Use `parser` and `plugins`.
-```javascript
-elixir(function(mix) {
-  mix.postcss('app.css', {
-    options: {
-      parser: require('sugarss')
-    },
-    plugins: [
-      require('postcss-nested')
-    ],
-  });
-});
+```js
+elixir( function( mix ) {
+    mix.postcss( "app.css", {
+        options: {
+            parser: require("sugarss")
+        },
+        plugins: [
+            require("postcss-nested")
+        ],
+    } );
+} );
 ```
 
 
@@ -75,24 +80,20 @@ This extension accept two parameters:
 Common options:
 
 * `options`: See [postcss common options](https://github.com/postcss/postcss#options).
-* `output`: destination's path
-* `plugins`: postcss's plugins.
+* `output`: destination"s path
+* `plugins`: postcss"s plugins.
 * `srcPath`: source's directory.
 * `sourcemaps`: enable source map.
 * `watchs`: additional watch directories.
 
 #### Default Value
-```javascript
+```js
 {
-  options: {},
-  output  : 'public/css',
-  plugins : [],
-  srcPath  : 'resources/assets/postcss/',
-  sourcemaps: true, //default value follow `elixir.config.sourcemaps`
-  watchs: [],
+    options    : {},
+    output     : "public/css",
+    plugins    : [],
+    srcPath    : "resources/assets/postcss/",
+    sourcemaps : true, // default value is the same as `elixir.config.sourcemaps`
+    watchs     : [],
 }
 ```
-
-
-## Contributing
-Welcome [PR](https://github.com/tureki/laravel-elixir-postcss/pulls) and play it :smile:
--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-'use strict';
+"use strict";
 
-var _      = require('underscore');
-var elixir = require('laravel-elixir');
-var gulp   = require('gulp');
-var gutil  = require('gulp-util');
-var rename = require('gulp-rename');
-var $      = require('gulp-load-plugins')();
-var config = elixir.config;
+var _      = require( "underscore" );
+var gutil  = require( "gulp-util" );
+var rename = require( "gulp-rename" );
+var $      = require( "gulp-load-plugins" )();
+var config = Elixir.config;
 
-elixir.extend('postcss', function(src, opts) {
+Elixir.extend( "postcss", function( src, opts ) {
+    var css = config.css;
+    var name = "postcss";
+    var notification = Elixir.Notification ?
+    Elixir.Notification :
+    require( "coldbox-elixir/Notification" );
 
-  var css = config.css;
-  var name = 'postcss';
-  var notification = elixir.Notification ? elixir.Notification : require('laravel-elixir/Notification');
+    opts = _.extend( {}, opts );
 
-  opts = _.extend({}, opts);
+    opts = _.extend( {
+        output     : config.publicPath + "/" + config.css.folder,
+        options    : {},
+        plugins    : [],
+        srcPath    : config.assetsPath + "/" + name,
+        sourcemaps : opts.sourcemaps ? opts.sourcemaps : config.sourcemaps,
+        watchs     : [],
+    }, opts );
 
-  opts = _.extend({
-    output: config.publicPath + '/' + config.css.folder,
-    options: {},
-    plugins: [],
-    srcPath: config.assetsPath + '/' + name,
-    sourcemaps: opts.sourcemaps ? opts.sourcemaps : config.sourcemaps,
-    watchs: [],
-  }, opts);
+    new Elixir.Task( name, function() {
+        var cssnano = css.cssnano ? css.cssnano.pluginOptions : undefined;
+        var srcPath = opts.srcPath + "/" + src;
 
-  new elixir.Task(name, function() {
+        var err = function( e ) {
+            // line break
+            console.log( "" );
+            console.log( gutil.colors.bgRed( "PostCSS - Error" ) );
+            console.log( "   - " + e.message );
+        };
 
-    var cssnano = css.cssnano ? css.cssnano.pluginOptions : undefined;
-    var srcPath = opts.srcPath + '/' + src;
+        if ( typeof this.recordStep !== "undefined" ) {
+            this.recordStep( "Post processing CSS" );
+            this.src = srcPath;
+            this.output = opts.output;
+        }
 
-    var err = function(e) {
-      // line break
-      console.log('');
-      console.log(gutil.colors.bgRed('PostCSS - Error'));
-      console.log('   - ' + e.message);
-    };
-
-    if(typeof this.recordStep !== 'undefined') {
-      this.recordStep('Post processing CSS');
-      this.src = srcPath;
-      this.output = opts.output;
-    }
-
-    return gulp.src(srcPath)
-      .pipe($.if(opts.sourcemaps, $.sourcemaps.init()))
-      .pipe($.postcss(opts.plugins, opts.options).on('error', err))
-      .pipe($.if(config.production, $.cssnano(cssnano)))
-      .pipe(rename({ extname: '.css' }))
-      .pipe($.if(opts.sourcemaps, $.sourcemaps.write('.')))
-      .pipe(gulp.dest(opts.output))
-      .pipe(new notification().message(name + ' Compiled!'));
-  })
-  .watch(_.union([opts.srcPath + '/**/*.css'], opts.watchs));
-
-});
+        return gulp.src( srcPath )
+            .pipe( $.if( opts.sourcemaps, $.sourcemaps.init() ) )
+            .pipe( $.postcss( opts.plugins, opts.options ).on( "error", err ) )
+            .pipe( $.if( config.production, $.cssnano( cssnano ) ) )
+            .pipe( rename( { extname: ".css" } ) )
+            .pipe( $.if( opts.sourcemaps, $.sourcemaps.write( "." ) ) )
+            .pipe( gulp.dest( opts.output ) )
+            .pipe( new notification().message( name + " Compiled!" ) );
+    } ).watch( _.union( [ opts.srcPath + "/**/*.css" ], opts.watchs ) );
+} );
